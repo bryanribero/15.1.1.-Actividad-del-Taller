860fe51bb2c4dbdda4da97144ee8b788c2fa3a29
Bryan <bryanribero@hotmail.com>
2024-10-15 18:52:17 -0300

    opciones en proceso

---
+++ js/cuerpo.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const opciones = document.querySelector('.cont-opciones');
+    const cuerpo = document.querySelector('.cont-datos');
+
+    const opcionesTexto = `
+    <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      <a href="#">Opción 1</a>
+    </div>
+    <div class="col">
+      <a href="#">Opción 2</a>
+    </div>
+    <div class="col">
+      <a href="#">Opción 3</a>
+    </div>
+    <div class="col">
+      <a href="#">Opción 4</a>
+    </div>
+  </div>
+</div>
+    `
+
+    opciones.innerHTML = opcionesTexto;
+
+
+    const cuerpoTexto = `
+    
+    `
+});
