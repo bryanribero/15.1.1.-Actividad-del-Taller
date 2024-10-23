d739eb5d588af0fb280a1096a2483687cd19889e
Bryan <bryanribero@hotmail.com>
2024-10-23 17:42:23 -0300

    termine la primera etapa de la tarea

--- js/cuerpo.js
+++ js/cuerpo.js
@@ -1,21 +1,53 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
 
-    const opciones = document.querySelector('.cont-opciones');
-    const cuerpo = document.querySelector('.cont-datos');
+  const contenedorCuerpo = document.querySelector('.contenedor-cuerpo');
 
-    const opcionesTexto = `
-    <div class="col-3 d-flex flex-column botones ">
-      <button class="btn ">Opción 1</button>
-      <button class="btn ">Opción 2</button>
-      <button class="btn ">Opción 3</button>
-      <button class="btn ">Opción 4</button>
+  
+  const opcionesTexto = `
+  <div class="col-md-3">
+  <div class="list-group botones">
+    <button class=" list-group-item-action btn">Opción 1</button>
+    <button class=" list-group-item-action btn">Opción 2</button>
+    <button class=" list-group-item-action btn">Opción 3</button>
+    <button class=" list-group-item-action btn">Opción 4</button>
+  </div>
+</div>
+  `;
+
+  
+  const cuerpoTexto = `
+  <div class="col-md-9">
+  <div class="row">
+    <div class="col-md-6 col-lg-4 mb-2 texto">
+      <p>Lorem ipsum dolor sit amet, commodo consequat. fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
-    `
+    <div class="col-md-6 col-lg-4 mb-2 texto">
+      <p>Phasellus eleifend semper tem...</p>
+    </div>
+    <div class="col-md-6 col-lg-4 mb-2 texto">
+      <p>Lorem ipsum dolor sit amet, commodo consequat. fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="col-md-6 col-lg-4 mb-2 texto">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+    </div>
+    <div class="col-md-6 col-lg-4 mb-2 texto">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+    </div>
+    <div class="col-md-6 col-lg-4 mb-2 texto">
+      <p>Lorem ipsum dolor sit amet, commodo consequat. fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="col-md-6 col-lg-4 mb-2 texto">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+    </div>
+    <div class="col-md-6 col-lg-4 mb-2 texto">
+      <p>Lorem ipsum dolor sit amet, commodo consequat. fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  </div>
+</div>
+  `;
 
-    opciones.innerHTML = opcionesTexto;
-
-
-    const cuerpoTexto = `
-    
-    `
+  contenedorCuerpo.innerHTML = `
+      ${opcionesTexto}
+      ${cuerpoTexto}
+  `;
 });
--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(){
     const contenedorNavBar = document.querySelector(".contenedor-navbar");
 
     const navBar = `
-    <nav class="navbar navbar-expand-lg bg-body-tertiary custom-navbar">
+    <nav class="navbar navbar-expand-lg custom-navbar">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
